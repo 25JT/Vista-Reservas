9775513533cdc37cfcabd324e1f979dc574dbe17
25JT <jhonierlucumimeza@gmail.com>
2025-04-09 11:55:48 -0500

    1.1 ACTUALIZACION DE LIBRERIAS

--- index.js
+++ index.js
@@ -88,11 +88,11 @@ app.post('/validar', function (req, res) {
                 }
     
                 let ultimoId = result[0] ? result[0].Id  : 1; // Incrementar el último ID o iniciar en 1
-                console.log("El último ID es:", ultimoId);
+                
                 valores.push(ultimoId); // Agregar el nuevo ID a los valores
                 const Idilt = ultimoId;
                 
-            console.log("Datos insertados correctamente", IdUlt);
+            
             return res.json({ success: true, message:`Reserva creada con éxito. No pierdas tu  ID de reserva ➡️: ${Idilt}`, id: Idilt });
             })
 
